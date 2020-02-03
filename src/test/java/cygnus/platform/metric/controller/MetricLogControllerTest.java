1e4668a5a6409322d151ff08a80e0dae11420e2b
Brian Ocasio <ocasiobrian15@gmail.com>
2020-02-02 23:49:59 -0500

    TICKET-1: Finalize tests

--- src/test/java/cygnus/platform/metric/controller/MetricLogControllerTest.java
+++ src/test/java/cygnus/platform/metric/controller/MetricLogControllerTest.java
@@ -1,8 +1,63 @@
 package cygnus.platform.metric.controller;
 
+import cygnus.platform.metric.dto.MetricLogRequest;
+import cygnus.platform.metric.dto.MetricLogResponse;
+import cygnus.platform.metric.model.MetricLog;
+import cygnus.platform.metric.service.MetricLogService;
+import cygnus.platform.metric.service.MetricService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MetricLogControllerTest {
+
+    private static final String METRIC_ID = "metricId";
+    private static final Integer ID = 1;
+    private static final Timestamp TIMESTAMP = Timestamp.from(Instant.now());
+    private static final BigDecimal VALUE = BigDecimal.ZERO;
+
+    @Mock
+    private MetricLogService metricLogService;
+
+    @Mock
+    private MetricService metricService;
+
+    private MetricLogController metricLogController;
+
+    @Before
+    public void setup() {
+        metricLogController = new MetricLogController(metricLogService, metricService);
+    }
+
+    @Test
+    public void testCreateLog() {
+        final MetricLogRequest metricLogRequest = new MetricLogRequest(METRIC_ID, BigDecimal.ZERO);
+        when(metricLogService.createMetricLog(any(), any())).thenReturn(createMetricLog());
+
+        final MetricLogResponse response = metricLogController.createMetricLog(METRIC_ID, metricLogRequest);
+        verify(metricService).validateMetricExistById(METRIC_ID);
+        Assert.assertEquals(METRIC_ID, response.getMetricId());
+        Assert.assertEquals(TIMESTAMP, response.getTimestamp());
+        Assert.assertEquals(VALUE, response.getValue());
+    }
+
+    private MetricLog createMetricLog() {
+        final MetricLog metricLog = new MetricLog();
+        metricLog.setMetricId(METRIC_ID);
+        metricLog.setTimestamp(TIMESTAMP);
+        metricLog.setValue(VALUE);
+        metricLog.setId(ID);
+        return metricLog;
+    }
 }
--- src/test/java/cygnus/platform/metric/controller/MetricStatisticsControllerTest.java
+++ src/test/java/cygnus/platform/metric/controller/MetricStatisticsControllerTest.java
@@ -1,10 +1,57 @@
 package cygnus.platform.metric.controller;
 
+import cygnus.platform.metric.dto.MetricStatisticsResponse;
+import cygnus.platform.metric.model.MetricStatistics;
+import cygnus.platform.metric.service.MetricService;
+import cygnus.platform.metric.service.MetricStatisticsService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MetricStatisticsControllerTest {
 
+    private static final String METRIC_ID = "metricID";
+    private static final BigDecimal MAX = BigDecimal.TEN;
+    private static final BigDecimal MIN = BigDecimal.ZERO;
+    private static final BigDecimal MEAN = BigDecimal.valueOf(5);
+    private static final BigDecimal MEDIAN= BigDecimal.valueOf(5);
 
+    @Mock
+    private MetricService metricService;
+
+    @Mock
+    private MetricStatisticsService metricStatisticsService;
+
+    private MetricStatisticsController metricStatisticsController;
+
+    @Before
+    public void setup() {
+        metricStatisticsController = new MetricStatisticsController(metricService, metricStatisticsService);
+    }
+
+    @Test
+    public void testGetStatistics() {
+        when(metricStatisticsService.findMetricStatisticsByMetricId(METRIC_ID)).thenReturn(createMetricStatistics());
+        metricStatisticsController.getMetricStatistics(METRIC_ID);
+        verify(metricService).validateMetricExistById(METRIC_ID);
+
+    }
+
+    private MetricStatistics createMetricStatistics() {
+        final MetricStatistics metricStatistics = new MetricStatistics();
+        metricStatistics.setMetricId(METRIC_ID);
+        metricStatistics.setMedian(MEDIAN);
+        metricStatistics.setMean(MEAN);
+        metricStatistics.setMinValue(MIN);
+        metricStatistics.setMaxValue(MAX);
+        return metricStatistics;
+    }
 }
